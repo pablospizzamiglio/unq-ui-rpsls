c0fef6c4d47757807d4113eda6afd5ca924c8281
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2022-07-06 17:14:54 -0300

    Move the card placeholders to the board

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -1,14 +1,20 @@
 import Card from "components/cards/Card";
+import Placeholder from "../images/mrx.png";
 import "./board.css";
+
+const placeholderCard = {
+  name: "placeholder",
+  src: Placeholder,
+};
 
 const Board = ({ cardA, cardB }) => {
   return (
     <div className="board">
       <div className="board-item">
-        <Card card={cardA} />
+        {cardA ? <Card card={cardA} /> : <Card card={placeholderCard} />}
       </div>
       <div className="board-item">
-        <Card card={cardB} />
+        {cardB ? <Card card={cardB} /> : <Card card={placeholderCard} />}
       </div>
     </div>
   );
