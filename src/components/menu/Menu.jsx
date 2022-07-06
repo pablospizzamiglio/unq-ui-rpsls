2e6c6ade85ea7a2afbea1c3505d81cf05b80b97c
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2022-07-06 20:53:51 -0300

    Add basic style to main menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,22 +1,17 @@
+import MenuButton from "components/buttons/MenuButton";
 import "./menu.css";
 
 const Menu = ({ onOnePlayerClick, onTwoPlayersClick }) => {
   return (
-    <div className="menu container">
-      <div className="menu-item">
+    <section className="menu">
+      <div className="menu-inner">
         <h1>Rock Paper Scissors Lizard Spock</h1>
-      </div>
-      <div className="menu-item">
         <div className="menu-button-group">
-          <button className="button" onClick={onOnePlayerClick}>
-            1 Player
-          </button>
-          <button className="button" onClick={onTwoPlayersClick}>
-            2 Players
-          </button>
+          <MenuButton onClick={onOnePlayerClick}>1 Player</MenuButton>
+          <MenuButton onClick={onTwoPlayersClick}>2 Players</MenuButton>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
