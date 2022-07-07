ea58384d7a03e776d000ec7fee05d96394cfb7a2
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2022-07-07 19:27:11 -0300

    Rename Card directory

--- src/components/buttons/CardButton.jsx
+++ src/components/buttons/CardButton.jsx
@@ -1,5 +1,5 @@
 import AnimatedButton from "components/buttons/AnimatedButton";
-import Card from "components/card/Card";
+import Card from "components/Card/Card";
 import "./button.css";
 
 const CardButton = ({ onClick, card, disabled }) => {
