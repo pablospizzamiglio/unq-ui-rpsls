b5941eecb1bf9dda33a861ed37dec1193566d6c5
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2022-07-07 20:57:27 -0300

    Remove unused code

--- src/components/Announcer/Announcer.jsx
+++ src/components/Announcer/Announcer.jsx
@@ -1,40 +1,6 @@
 import "./Announcer.css";
 
-import { useEffect, useState } from "react";
-
-export const wait = (ms) =>
-  new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, ms);
-  });
-
-export const useTypedMessage = (message) => {
-  const [typedMessage, setTypedMessage] = useState("");
-
-  useEffect(() => {
-    setTypedMessage("");
-
-    if (message.length) {
-      (async () => {
-        let visibleMessage = "";
-        for (let i = 0; i < message.length; i++) {
-          await wait(25);
-
-          visibleMessage = visibleMessage + message[i];
-
-          setTypedMessage(visibleMessage);
-        }
-      })();
-    }
-  }, [message]);
-
-  return typedMessage;
-};
-
 const Announcer = ({ message }) => {
-  // const typedMessage = useTypedMessage(message);
-
   return (
     <div className="announcer border">
       <div className="announcer-item">
