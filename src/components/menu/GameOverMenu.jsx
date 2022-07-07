953c7943795e991aef1e15a65c1947a00db7a917
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2022-07-07 02:26:20 -0300

    Add GameOverMenu

---
+++ src/components/menu/GameOverMenu.jsx
@@ -0,0 +1,21 @@
+import MenuButton from "components/buttons/MenuButton";
+import Donut from "components/icons/Donut";
+import "./menu.css";
+
+const GameOverMenu = ({ onPlayAgainClick, onMainMenuClick, message }) => {
+  return (
+    <section className="menu">
+      <div className="menu-inner">
+        <Donut />
+        <h1 style={{ textTransform: "uppercase" }}>{message}</h1>
+        <h1>Game Over</h1>
+        <div className="menu-button-group">
+          <MenuButton onClick={onPlayAgainClick}>Play Again</MenuButton>
+          <MenuButton onClick={onMainMenuClick}>Main Menu</MenuButton>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default GameOverMenu;
